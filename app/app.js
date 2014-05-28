57f55b8255e19f5490878165bb345d0de0cbe248
xlenz <xlenz7@ua.fm>
2014-05-28 14:10:10 +0300

    Andrey's fix

--- app/app.js
+++ app/app.js
@@ -1,38 +1,75 @@
 'use strict';
 var responses = [];
-(function () {
-  var app = angular.module('dimaSpa', ['dimaSpa-directives']);
+(function() {
+    var app = angular.module('dimaSpa', ['dimaSpa-directives']);
 
-  app.controller('InsertSelectController', [
-    '$http',
-    function ($http) {
-      var queries = this;
-      queries.responses = [];
-      queries.amount = '';
+    app.controller('InsertSelectController', ['$http', '$scope', '$q',
+        function($http, $scope, $q) {
 
-      queries.submit = function () {
-        var counter = queries.amount;
-        while (counter > 0) {
-          counter--;
-          console.log(counter);
-          $http({
-            method: 'POST',
-            data: {
-              name: 'something'
-            },
-            url: '/insertSelect'
-          }).success(function (data) {
-            var usersCount = data.json[0]['COUNT(id)'];
-            console.log(usersCount);
-            responses.push('Success, ' + usersCount);
-            //queries.responses.push('Success, ' + usersCount);
-          }).error(function (data) {
-            console.error(data);
-            responses.push('Error, ' + data.message || data.error);
-            //queries.responses.push('Error, ' + data.message || data.error);
-          });
+            $scope.responses = [];
+            $scope.amount = '';
+
+            $scope.submit = function(value) {
+                if ($scope.insertSelect.$valid) {
+                    post(value);
+                };
+                /*var counter = $scope.amount;
+                while (counter > 0) {
+                    counter--;
+                    console.log(counter);
+                    $http({
+                        method: 'POST',
+                        data: {
+                            name: 'something'
+                        },
+                        url: '/insertSelect'
+                    }).success(function(data) {
+                        var usersCount = data.json[0]['COUNT(id)'];
+                        console.log(usersCount);
+                        //responses.push('Success, ' + usersCount);
+                        $scope.responses.push('Success, ' + usersCount);
+                    }).error(function(data) {
+                        console.error(data);
+                        //responses.push('Error, ' + data.message || data.error);
+                        $scope.responses.push('Error, ' + data.message || data.error);
+                    });
+                }*/
+            };
+
+
+            var post = function(value) {
+
+                var arr = new Array();
+
+                for (var i = 0; i < value; i++) {
+                    arr.push($http({
+                        method: 'POST',
+                        data: {
+                            name: 'something'
+                        },
+                        url: '/insertSelect'
+                    }))
+                };
+
+                $q.all(arr).then(
+                    function(data) {
+                        angular.forEach(data, function(item) {
+                            $scope.responses.push({
+                              id: item.data.json[0]['COUNT(id)']
+                            });
+                        });
+
+                        console.log($scope.responses);
+                    },
+                    function(error) {
+
+                    }
+                )
+
+            }
+
+
+
         }
-      };
-    }
-  ]);
+    ]);
 })();
